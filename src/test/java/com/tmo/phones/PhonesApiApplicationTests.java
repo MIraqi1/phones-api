9f988d1c5c52774fbe20fb9b73ec00def3c5a753
mandah-a11y <mandakhgerel.ganbold1@t-mobile.com>
2020-11-19 08:39:34 -0800

    added basic phone details

--- src/test/java/com/tmo/phones/PhonesApiApplicationTests.java
+++ src/test/java/com/tmo/phones/PhonesApiApplicationTests.java
@@ -1,13 +1,33 @@
 package com.tmo.phones;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PhonesApiApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void getPhoneDetails_returnsPhoneDetails() {
+
+		// access and api and receive details about a phone
+		ResponseEntity<Phone> response = restTemplate.getForEntity("/phones/iphone", Phone.class);
+
+		// from AssertJ
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody().getMake()).isEqualTo("iphone");
+		assertThat(response.getBody().getModel()).isEqualTo("iphone");
+	}
 }
